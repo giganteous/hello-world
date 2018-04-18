d66f1084e7bc2ed6f0997f459523c1628e8d7cf0
Kai Storbeck <kai@xs4all.nl>
2018-04-18 21:17:25 +0200

    really fail now

--- hello/test_hello.py
+++ hello/test_hello.py
@@ -14,5 +14,5 @@ class TestHello(unittest.TestCase):
         """sample test with unicode"""
         self.assertEqual(hello(u'world'), u'hello world')
 
-    def fail_miserably(self):
+    def test_fails(self):
         self.fail("Instant misery!")
